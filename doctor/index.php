f0a0678df9f85c56f48918c568f24e35e0e08d96
KAVEESHA NANAYAKKARA <kasmirakaveesha@gmail.com>
2024-03-09 11:18:39 +0530

    Version 3
    Make changes to Doctor Dashboard

---
+++ doctor/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/animations.css">  
+    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/admin.css">
+        
+    <title>Dashboard</title>
+    <style>
+        .dashbord-tables,.doctor-heade{
+            animation: transitionIn-Y-over 0.5s;
+        }
+        .filter-container{
+            animation: transitionIn-Y-bottom  0.5s;
+        }
+        .sub-table,#anim{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+        .doctor-heade{
+            animation: transitionIn-Y-over 0.5s;
+        }
+    </style>
+    
+    
+</head>
+<body>
+    <?php
+
+
+
+    session_start();
+
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
+            header("location: ../login.php");
+        }else{
+            $useremail=$_SESSION["user"];
+        }
+
+    }else{
+        header("location: ../login.php");
+    }
+    
+
+    //import database
+    include("../connection.php");
+    $userrow = $database->query("select * from doctor where docemail='$useremail'");
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["docid"];
+    $username=$userfetch["docname"];
+
+
+    //echo $userid;
+    //echo $username;
+    
+    ?>
+    <div class="container">
+        <div class="menu">
+            <table class="menu-container" border="0">
+                <tr>
+                    <td style="padding:10px" colspan="2">
+                        <table border="0" class="profile-container">
+                            <tr>
+                                <td width="30%" style="padding-left:20px" >
+                                    <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
+                                </td>
+                                <td style="padding:0px;margin:0px;">
+                                    <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
+                                    <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
+                                </td>
+                            </tr>
+                    </table>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-dashbord menu-active menu-icon-dashbord-active" >
+                        <a href="index.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">Dashboard</p></a></div></a>
+                    </td>
+                </tr>
+                <tr class="menu-row">
+                    <td class="menu-btn menu-icon-appoinment">
+                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">My Appointments</p></a></div>
+                    </td>
+                </tr>
+                
+             
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-patient">
+                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">My Patients</p></a></div>
+                    </td>
+                </tr>
+              
+                
+            </table>
+        </div>
+        <div class="dash-body" style="margin-top: 15px">
+            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;" >
+                        
+                        <tr >
+                            
+                            <td colspan="1" class="nav-bar" >
+                            <p style="font-size: 23px;padding-left:12px;font-weight: 600;margin-left:20px;">     Dashboard</p>
+                          
+                            </td>
+                            <td width="25%">
+
+                            </td>
+                            <td width="15%">
+                                <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+                                    Today's Date
+                                </p>
+                                <p class="heading-sub12" style="padding: 0;margin: 0;">
+                                    <?php 
+                                date_default_timezone_set('Asia/Kolkata');
+        
+                                $today = date('Y-m-d');
+                                echo $today;
+
+
+                                $patientrow = $database->query("select  * from  patient;");
+                                $doctorrow = $database->query("select  * from  doctor;");
+                                $appointmentrow = $database->query("select  * from  appointment where appodate>='$today';");
+                                $schedulerow = $database->query("select  * from  schedule where scheduledate='$today';");
+
+
+                                ?>
+                                </p>
+                            </td>
+                            <td width="10%">
+                                <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
+                            </td>
+        
+        
+                        </tr>
+                <tr>
+                    <td colspan="4" >
+                        
+                    <center>
+                    <table class="filter-container doctor-header" style="border: none;width:95%" border="0" >
+                    <tr>
+                        <td >
+                            <h3>Welcome!</h3>
+                            <h1><?php echo $username  ?>.</h1>
+                            <p>Thanks for joinnig with us. We are always trying to get you a complete service<br>
+                            You can view your dailly schedule, Reach Patients Appointment at home!<br><br>
+                            </p>
+                            <a href="appointment.php" class="non-style-link"><button class="btn-primary btn" style="width:30%">View My Appointments</button></a>
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    </table>
+                    </center>
+                    
+                </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <table border="0" width="100%"">
+                            <tr>
+                                <td width="50%">
+
+                                    
+
+
+
+
+                                    <center>
+                                        <table class="filter-container" style="border: none;" border="0">
+                                            <tr>
+                                                <td colspan="4">
+                                                    <p style="font-size: 20px;font-weight:600;padding-left: 12px;">Status</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="width: 25%;">
+                                                    <div  class="dashboard-items"  style="padding:20px;margin:auto;width:95%;display: flex">
+                                                        <div>
+                                                                <div class="h1-dashboard">
+                                                                    <?php    echo $doctorrow->num_rows  ?>
+                                                                </div><br>
+                                                                <div class="h3-dashboard">
+                                                                    All Doctors &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                </div>
+                                                        </div>
+                                                         
+                                                </td>
+                                                <td style="width: 25%;">
+                                                    <div  class="dashboard-items"  style="padding:20px;margin:auto;width:95%;display: flex;">
+                                                        <div>
+                                                                <div class="h1-dashboard">
+                                                                    <?php    echo $patientrow->num_rows  ?>
+                                                                </div><br>
+                                                                <div class="h3-dashboard">
+                                                                    All Patients &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                </div>
+                                                        </div>
+                                                  
+                                                </td>
+                                                </tr>
+                                                
+                                        </table>
+                                    
+                                <td>
+
+
+                                        </div>
+                                 
+
+
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                <tr>
+            </table>
+        </div>
+    </div>
+
+
+</body>
+</html>
