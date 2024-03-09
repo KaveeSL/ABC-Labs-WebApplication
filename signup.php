8f7c6b252aa10fb9046745d0d28bf4bb3214b0a1
KAVEESHA NANAYAKKARA <kasmirakaveesha@gmail.com>
2024-03-09 09:04:40 +0530

    Version 1
    Added homepage and login,register.

---
+++ signup.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="css\footer.css">
+        
+    <title>Sign Up</title>
+    
+</head>
+<body>
+<?php
+
+
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+
+
+if($_POST){
+
+    
+
+    $_SESSION["personal"]=array(
+        'fname'=>$_POST['fname'],
+        'lname'=>$_POST['lname'],
+        'address'=>$_POST['address'],
+        'nic'=>$_POST['nic'],
+        'dob'=>$_POST['dob']
+    );
+
+
+    print_r($_SESSION["personal"]);
+    header("location: create-account.php");
+
+
+
+
+}
+
+?>
+
+
+    <center>
+    <div class="container">
+        <table border="0">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Let's Get Started</p>
+                    <p class="sub-text">Add Your Personal Details to Continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="name" class="form-label">Name: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                </td>
+                <td class="label-td">
+                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="address" class="form-label">Address: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="address" class="input-text" placeholder="Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="nic" class="form-label">NIC: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="dob" class="form-label">Date of Birth: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="date" name="dob" class="input-text" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                </td>
+            </tr>
+
+            <tr>
+              
+                <td>
+                    <input type="submit" value="Next" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<footer id="picassoFooter">
+        <div class="footer-navigation">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-contact">
+            <p>Email Us: abc@labspvtltd.com</p>
+            <p>Call Us: 045 456 7890</p>
+        </div>
+        <div class="footer-social">
+            <h3>Follow Us</h3>
+            <div class="social-icons">
+                <a href="https://facebook.com" target="_blank" class="social-icon">FB</a>
+                <a href="https://twitter.com" target="_blank" class="social-icon">TW</a>
+                <a href="https://instagram.com" target="_blank" class="social-icon">IG</a>
+            </div>
+        </div>
+        <div class="footer-art">
+            <canvas id="picassoCanvas"></canvas>
+        </div>
+    </footer>
+
+<!--footer ABC LABS text-->
+    <script>
+        window.onload = function() {
+            var canvas = document.getElementById('picassoCanvas');
+            if (canvas.getContext) {
+                var ctx = canvas.getContext('2d');
+    
+                // Set canvas size
+                canvas.width = 200;
+                canvas.height = 200;
+    
+                // Add text "ABC LABS" in white color and bold
+                ctx.fillStyle = "#FFFFFF";
+                ctx.font = "bold 30px Arial";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillText("| ABC LABS |", 100, 100);
+            }
+        };
+    </script>
+</body>
+</html>
